d4a51bdb452ce44acced3d0112f724db202e4a95
Mathys <128370616+mathyspqr@users.noreply.github.com>
2025-04-10 15:03:28 +0200

    test 3.b ne passe pas

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-export function add(numbers) {
+// export function add(numbers) {
     
+//     if(numbers === '') {
+//         return 0;
+//     }
+    
+//     const nums = numbers.split(',');
+    
+//     if (nums.length === 1) {
+//         return parseInt(nums[0]);
+//     }
+    
+//     if (nums.length === 2) {
+//         return parseInt(nums[0]) + parseInt(nums[1]);
+//     }
+// }
+
+export function add(numbers) {
     if(numbers === '') {
         return 0;
     }
+
+    numbers = "1,\n"
     
-    const nums = numbers.split(',');
-    
-    if (nums.length === 1) {
-        return parseInt(nums[0]);
-    }
-    
-    if (nums.length === 2) {
-        return parseInt(nums[0]) + parseInt(nums[1]);
-    }
+    return numbers
+        .replace(/\n/g, ',')  
+        .split(',')
+        .reduce((sum, num) => sum + parseInt(num), 0);
 }
--- main.test.ts
+++ main.test.ts
@@ -12,3 +12,11 @@ test("single number returns the number", () => {
 test("two numbers returns their sum", () => {
     expect(add("1,2")).toBe(3);
 });
+
+test("handles multiple numbers", () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+});
+
+test("handles newlines as delimiters", () => {
+    expect(add("1\n2,3")).toBe(6);
+});
